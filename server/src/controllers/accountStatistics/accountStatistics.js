1b7b575cd01cd29655b821e9e7a390c9ab6da21a
sivakrish1 <sivapalaparthi003@gmail.com>
2026-07-14 19:20:56 +0530

    security: isolate account aggregation statistics to req.user._id

--- server/src/controllers/accountStatistics/accountStatistics.js
+++ server/src/controllers/accountStatistics/accountStatistics.js
@@ -1,48 +1,59 @@
+
+
+const mongoose = require("mongoose");
 const Expense = require("../../models/Expense");
 const Income = require("../../models/Income");
 
-const accountStatsCtrl = async(req,res) =>{
-    const sampleExpense = await Expense.findOne();
-console.log(typeof sampleExpense.amount); // should be "number"
-
-
+const accountStatsCtrl = async (req, res) => {
     try {
-     const expenseStas = await Expense.aggregate([
-        {$match: { amount : {$gte: 0}}},
-        {
-            $group:{
-                _id: null,
-                totalExp: { $sum : "$amount"},
-                minExp: {$min : "$amount"},
-                averageExp : {$avg : "$amount"},
-                maxExp : {$max : "$amount"},
-                totalRecordsExp : {$sum : 1}
+        const userId = new mongoose.Types.ObjectId(req?.user?._id);
+
+        const expenseStats = await Expense.aggregate([
+            {
+                $match: { 
+                    user: userId, // Security: only aggregate this user's records
+                    amount: { $gte: 0 } 
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalExp: { $sum: "$amount" },
+                    minExp: { $min: "$amount" },
+                    averageExp: { $avg: "$amount" },
+                    maxExp: { $max: "$amount" },
+                    totalRecordsExp: { $sum: 1 }
+                }
             }
-        }
-    ])
+        ]);
 
-    const incomeStas = await Income.aggregate([
-        {$match: { amount : {$gte: 0}}},
-        {
-            $group:{
-                _id: null,
-                totalIncome: { $sum : "$amount"},
-                minIncome: {$min : "$amount"},
-                averageIncome : {$avg : "$amount"},
-                maxIncome : {$max : "$amount"},
-                totalRecordsIncome : {$sum : 1}
+        const incomeStats = await Income.aggregate([
+            {
+                $match: { 
+                    user: userId, // Security: only aggregate this user's records
+                    amount: { $gte: 0 } 
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalIncome: { $sum: "$amount" },
+                    minIncome: { $min: "$amount" },
+                    averageIncome: { $avg: "$amount" },
+                    maxIncome: { $max: "$amount" },
+                    totalRecordsIncome: { $sum: 1 }
+                }
             }
-        }
-    ])
+        ]);
 
-    return res.status(200).json({
-      expenseStats: expenseStas[0] || {},
-      incomeStats: incomeStas[0] || {}
-    });
+        return res.status(200).json({
+            expenseStats: expenseStats[0] || {},
+            incomeStats: incomeStats[0] || {}
+        });
 
-   } catch (error) {
-      return res.json(error.message)
-   }
-}
+    } catch (error) {
+        return res.json(error.message)
+    }
+};
 
-module.exports = accountStatsCtrl
+module.exports = accountStatsCtrl;
